57c88512ee217e64191abfe5dcce873ef8ee2993
LuluBazan2001 <142625929+LuluBazan2001@users.noreply.github.com>
2025-02-16 19:10:16 -0300

    Creación de Servidor Express, y Estructura del Proyecto

---
+++ server.js
@@ -0,0 +1,14 @@
+/*CODIGO DE PRUEBA PARA CORRER SERVIDOR EN NODE.JS*/
+// const express = require('express');
+// const app = express();
+// const PORT = 3000;
+
+// // Ruta principal
+// app.get('/', (req, res) => {
+//     res.send('¡Hola, Express!');
+// });
+
+// // Iniciar el servidor
+// app.listen(PORT, () => {
+//     console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// });
