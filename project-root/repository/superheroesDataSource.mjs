03a27be63fb58b1c156ed6af5ac90b3184e0d5c7
Juvazquez <juvazquez616@gmail.com>
2025-02-17 18:31:04 -0300

    Se agrego contenido a la estructura del proyecto

---
+++ project-root/repository/superheroesDataSource.mjs
@@ -0,0 +1,8 @@
+
+export default class SuperheroesDataSource {
+    // Metodo abstracto para obtener todos los superheroes
+
+    obtenerTodos() {
+        throw new Error('Este metodo debe estar implementado por la subclase');
+    }
+}
